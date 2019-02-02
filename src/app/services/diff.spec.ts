f84aca1ee5ddb5846d0ce46b79e1ca7eb755bbc3
tomastrajan <tomas.trajan@gmail.com>
2019-02-02 15:02:01 +0100

    fix(diff): fix tests

--- src/app/services/diff.spec.ts
+++ src/app/services/diff.spec.ts
@@ -1,4 +1,17 @@
+import { resolve } from 'path';
+import { writeJSON } from 'fs-extra';
+
 import { createHtmlDiff } from '@services/diff';
+
+const TEST_FILE = resolve('./test', 'some-file.json');
+
+beforeEach(async () => {
+  await writeJSON(TEST_FILE, { prop: 'changed-value' }, { spaces: 2 });
+});
+
+afterEach(async () => {
+  await writeJSON(TEST_FILE, { prop: 'value' }, { spaces: 2 });
+});
 
 test('creates a file diff', async () => {
   const res = await createHtmlDiff('anything/test.git', '.', 'name');
